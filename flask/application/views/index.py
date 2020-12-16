cf5b25f38c31c8818a15ea9d4edf8082a31842b8
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2020-12-17 04:02:42 +1100

    Added support for static typing.

--- flask/application/views/index.py
+++ flask/application/views/index.py
@@ -1,10 +1,12 @@
+""" Flask-related imports """
 from flask import render_template
 
+""" nate.dev-related imports """
 from application import app
 from application.helpers import md2html
 
 @app.route("/")
-def about():
-    html = md2html("application/static/content/about.md")
+def about() -> None:
+    html : str = md2html("application/static/content/about.md")
     return render_template("index.html", html=html)
 
