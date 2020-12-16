93e31848cb0f06e0fdf6f41d541776fb1a8b2fdd
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2020-12-17 04:00:48 +1100

    Added support for static typing and minor documentation.

--- flask/application/views/cv.py
+++ flask/application/views/cv.py
@@ -1,9 +1,11 @@
+""" Flask-related imports """
 from flask import render_template
 
+""" nate.dev-related imports """
 from application import app
 from application.helpers import md2html
 
 @app.route("/cv")
-def cv():
-    html = md2html("application/static/content/cv.md")
+def cv() -> None:
+    html : str = md2html("application/static/content/cv.md")
     return render_template("cv.html", html=html)
