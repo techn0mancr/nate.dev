688db74348f3a5c99c70e1d6ea93db7417cc5a9e
techn0mancr <17984241+techn0mancr@users.noreply.github.com>
2021-01-17 19:33:27 +1100

    Added error handling code.

--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -1,5 +1,9 @@
+""" Flask-related imports """
 from flask import Flask
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+
+""" nate.dev-related imports """
+from application.helpers import apology
 
 # Configure application
 app = Flask(__name__)
@@ -8,12 +12,13 @@ app.config.from_object('application.instances.development')
 
 from application import views
 
-#def errorhandler(error):
-#    """ Handle error """
-#    if not isinstance(error, HTTPException):
-#        error = InternalServerError()
-#    return apology(error.name, error.code)
+def errorhandler(error):
+    """ Handles errors """
+    if not isinstance(error, HTTPException):
+        error = InternalServerError()
+
+    return apology(error.name, error.code)
 
 # Listen for errors
-#for code in default_exceptions:
-#    app.errorhandler(code)(errorhandler)
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
